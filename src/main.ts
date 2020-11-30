dc38a681f04dcbce51aecec8640f9b186ff7ba06
supergrecko <me@supergrecko.com>
2020-11-30 21:15:32 +0100

    fix: fix issue where directory prefixes would malform if prefix was specified (#1)
    * fix: fix issue where non-prefixed path would fail
    
    * fix: fix incorrect s3 key names
    
    * chore: upload dist

--- src/main.ts
+++ src/main.ts
@@ -64,13 +64,8 @@ async function run() {
         // Strip the cwd, a slash and the host directory from S3 Key
         //   /home/runner/work/$folder
         //                    ^
-        let cwdLength = process.cwd().length + config.directory.length + 2
-        const prefix = `${config.awsKeyPrefix}/`
-        if (config.awsKeyPrefix === '') {
-          // If there is no key prefix, we strip the initial slash as well.
-          cwdLength++
-        }
-        const s3Key = `${prefix}${file.substr(cwdLength)}`
+        let cwdLength = process.cwd().length + config.directory.length + 1
+        const s3Key = `${config.awsKeyPrefix}${file.substr(cwdLength)}`
 
         return await s3.upload({
           Bucket: config.bucket,
